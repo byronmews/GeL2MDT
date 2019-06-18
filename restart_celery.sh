7fdd98ee77186b1e40d4366dd0f29ef31c85176e
byronmews <ptk.lmb55@gmail.com>
2019-06-18 10:06:38 +0100

    cron restart celery tasks

--- restart_celery.sh
+++ restart_celery.sh
@@ -2,4 +2,4 @@ gel2mdt=/home/genseqservadmin/WL_GEL2MDT/wl_instance
 docker=/usr/local/bin
 $docker/docker-compose -p wl_g2m -f $gel2mdt/docker-compose-prod.yml restart celery
 $docker/docker-compose -p wl_g2m -f $gel2mdt/docker-compose-prod.yml restart celery-beat
-echo completed: `date`
+echo completed:`date`
